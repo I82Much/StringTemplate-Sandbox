f265ef97617db95cc0d1c1598cef94dd3f11b10e
Nick Dunn <ndunn@mitre.org>
2011-04-30 12:59:37 -0400

    JSON hooked up now.

--- src/net/developmentality/StringTemplate/StringTemplateTestServlet.java
+++ src/net/developmentality/StringTemplate/StringTemplateTestServlet.java
@@ -1,5 +1,6 @@
 package net.developmentality.StringTemplate;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
@@ -7,8 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateErrorListener;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import antlr.RecognitionException;
+import antlr.TokenStreamRecognitionException;
 
 
 @SuppressWarnings("serial")
@@ -37,8 +42,7 @@ public class StringTemplateTestServlet extends HttpServlet {
 		JSONObject data = null;
 		try {
 			data = new JSONObject(json);
-	    }
-	    
+	    }	    
 		catch (JSONException je)
 	    {
 			resp.setContentType("text/html");
@@ -49,12 +53,68 @@ public class StringTemplateTestServlet extends HttpServlet {
 			return;
 	    }
 		
-		StringTemplate template = new StringTemplate(content);
+		StringTemplate template = null;
+		try {
+			template = new StringTemplate(content);
+		}
+		catch (Exception e) {
+			resp.setContentType("text/html");
+			resp.getWriter().println("There's an error in your StringTemplate syntax.  Please see <a href=\"http://www.stringtemplate.org\">StringTemplate website</a> for more information");
+			resp.getWriter().println("<pre>");
+			e.printStackTrace(resp.getWriter());
+			resp.getWriter().println("</pre>");
+			return;
+		}
+		
+		// The JSON provided by the user powers the 
 		template.setArgumentContext(data);
-//		template.setAttribute("name", "Frank");
 		
 		resp.getWriter().println(template.toString());
-		
-		resp.getWriter().println(data);
 	}
+	
+	
+	// Borrowed, modified from the STST project written by John Snyders
+//	private class STErrorListener implements StringTemplateErrorListener
+//    {
+//        public void error(String msg, Throwable e)
+//        {
+//            if (e == null)
+//            {
+//                String format = resources.getString("Error");
+//                logError(MessageFormat.format(format, msg));
+//            }
+//            else if (e instanceof TokenStreamRecognitionException)
+//            {
+//                TokenStreamRecognitionException tsr = (TokenStreamRecognitionException)e;
+//                RecognitionException r = tsr.recog;
+//                String format = resources.getString("RecognitionExceptionError");
+//                logError(MessageFormat.format(format, msg, e.getLocalizedMessage(), 
+//                        r.getLine(), r.getColumn()));
+//            }
+//            else if (e instanceof RecognitionException)
+//            {
+//                RecognitionException r = (RecognitionException)e;
+//                String format = resources.getString("RecognitionExceptionError");
+//                logError(MessageFormat.format(format, msg, e.getLocalizedMessage(), 
+//                        r.getLine(), r.getColumn()));
+//            }
+//            else
+//            {
+//                String format = resources.getString("ExceptionError");
+//                logError(MessageFormat.format(format, msg, e.getLocalizedMessage()));
+//                
+//            }
+//        }
+//
+//        public void warning(String msg)
+//        {
+//            String format = resources.getString("Warning");
+//            logError(MessageFormat.format(format, msg));
+//        }
+//    }
+//
+//    private void logError(String message)
+//    {
+//        System.err.println(message);
+//    }
 }
