1698d6e7e1e0fd5ed7edd3560b0889b0f168c21c
Nick Dunn <ndunn@mitre.org>
2011-04-30 12:08:05 -0400

    Getting close to actually having servlet work.

--- src/net/developmentality/StringTemplate/StringTemplateTestServlet.java
+++ src/net/developmentality/StringTemplate/StringTemplateTestServlet.java
@@ -1,11 +1,20 @@
 package net.developmentality.StringTemplate;
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.antlr.stringtemplate.StringTemplate;
 
 @SuppressWarnings("serial")
 public class StringTemplateTestServlet extends HttpServlet {
+	
+	private static final Logger log = Logger.getLogger(StringTemplateTestServlet.class.getName());
+
+	
+	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		
@@ -14,5 +23,15 @@ public class StringTemplateTestServlet extends HttpServlet {
 		
 		resp.getWriter().println(template.toString());
 		
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		String content = req.getParameter("content");
+        log.info("Content: " + content);		
+//        resp.sendRedirect("/stringtemplate.html");
+        
+        resp.getWriter().println(content);
 	}
 }
